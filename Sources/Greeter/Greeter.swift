5021c0190cf833aa58250a20737c1d0d7f5ed028
Coast <coast@upcoast.ca>
2026-07-17 20:27:15 -0700

    feat: implement F-M1 to green (TDD green)

--- Sources/Greeter/Greeter.swift
+++ Sources/Greeter/Greeter.swift
@@ -3,10 +3,10 @@ public struct Greeter {
     public init() {}
 
     public func greet(name: String) -> String {
-        ""
+        "Hello, \(name)!"
     }
 
     public func greet() -> String {
-        ""
+        "Hello, world!"
     }
 }
