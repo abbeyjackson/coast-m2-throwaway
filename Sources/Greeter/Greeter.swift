208180deb061c06a78b424b06bad3542df7bf477
Coast <coast@upcoast.ca>
2026-07-17 19:35:07 -0700

    tests: failing requirement tests for F-M1 (TDD red)

---
+++ Sources/Greeter/Greeter.swift
@@ -0,0 +1,14 @@
+/// Produces greeting strings for a given name.
+public struct Greeter {
+    public init() {}
+
+    /// Returns a greeting for the given name.
+    public func greet(name: String) -> String {
+        "TODO"
+    }
+
+    /// Returns a default greeting.
+    public func greet() -> String {
+        "TODO"
+    }
+}
